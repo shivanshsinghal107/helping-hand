43ef9fd2f3ccafab37068aaf1e2db91011fd8f19
Shivansh Singhal <shivanshsinghal107@gmail.com>
2021-05-01 14:16:54 +0530

    Added some code of lines for session

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import requests
 import datetime
 
 from flask import Flask, request, render_template, redirect
+from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///database.db"))
