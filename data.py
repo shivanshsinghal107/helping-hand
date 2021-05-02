039339821e3f2f2c560ea44f73eea751cb8a5d76
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2021-05-02 19:33:44 +0530

    Changed the directory system according to flask

--- data.py
+++ data.py
@@ -8,12 +8,14 @@ engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///database.db"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def make_tables():
-    db.execute('''CREATE TABLE IF NOT EXISTS givehelp (username VARCHAR(16) NOT NULL, password VARCHAR(64) NOT NULL, join_date text,
-                PRIMARY KEY(username))''')
-    db.execute('''CREATE TABLE IF NOT EXISTS seekhelp (mail VARCHAR(64) NOT NULL, username VARCHAR(16) NOT NULL, FOREIGN KEY(username)
-                REFERENCES users(username) ON DELETE CASCADE ON UPDATE CASCADE, PRIMARY KEY(mail, username), UNIQUE(mail, username))''')
+    db.execute('''CREATE TABLE IF NOT EXISTS gethelp (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL,
+                  state VARCHAR(64) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30), requirements text,
+                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
+    db.execute('''CREATE TABLE IF NOT EXISTS giveleads (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL,
+                  state VARCHAR(64) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30), requirements text,
+                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
     db.commit()
     db.close()
 
-if __name__ == "main":
+if __name__ == "__main__":
     make_tables()
