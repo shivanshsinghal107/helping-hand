daf4f23c410831933fba12f83268918b774012af
Shivansh Singhal <shivanshsinghal107@gmail.com>
2021-05-03 16:45:48 +0530

    Updated the primary key column for heroku deployment

--- data.py
+++ data.py
@@ -8,10 +8,10 @@ engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///database.db"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def make_tables():
-    db.execute('''CREATE TABLE IF NOT EXISTS gethelp (id INTEGER PRIMARY KEY AUTOINCREMENT, unique_id CHAR(8) NOT NULL,
+    db.execute('''CREATE TABLE IF NOT EXISTS gethelp (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
                   name VARCHAR(255) NOT NULL, state VARCHAR(64) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30),
                   requirements VARCHAR(255), bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
-    db.execute('''CREATE TABLE IF NOT EXISTS giveleads (id INTEGER PRIMARY KEY AUTOINCREMENT, unique_id CHAR(8) NOT NULL,
+    db.execute('''CREATE TABLE IF NOT EXISTS giveleads (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
                   name VARCHAR(255) NOT NULL, state VARCHAR(64) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30),
                   requirements VARCHAR(255), bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
     db.commit()
