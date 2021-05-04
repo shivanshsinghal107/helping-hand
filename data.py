5fb04c8109ee300b9e867548644104e594d3c372
Shivansh Singhal <shivanshsinghal107@gmail.com>
2021-05-04 15:45:51 +0530

    Removed the foreign key relation and cascade for the feedback table

--- data.py
+++ data.py
@@ -10,12 +10,12 @@ db = scoped_session(sessionmaker(bind=engine))
 def make_tables():
     db.execute('''CREATE TABLE IF NOT EXISTS gethelp (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
                   name VARCHAR(255) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30), requirements VARCHAR(255),
-                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text, UNIQUE(unique_id))''')
+                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
     db.execute('''CREATE TABLE IF NOT EXISTS giveleads (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
                   name VARCHAR(255) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30), requirements VARCHAR(255),
-                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text, UNIQUE(unique_id))''')
+                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
     db.execute('''CREATE TABLE IF NOT EXISTS feedback (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
-                  note text NOT NULL, FOREIGN KEY(unique_id) REFERENCES gethelp(unique_id) ON DELETE CASCADE ON UPDATE CASCADE)''')
+                  note text NOT NULL)''')
     db.execute('''CREATE TABLE IF NOT EXISTS helped (count INTEGER)''')
     db.execute("INSERT INTO helped (count) VALUES (:count)", {"count": 0})
     db.commit()
