92e951be13120b5f6db93e6c591ea79effd0ca28
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2021-05-05 17:38:22 +0530

    Added buffer table

--- data.py
+++ data.py
@@ -14,6 +14,9 @@ def make_tables():
     db.execute('''CREATE TABLE IF NOT EXISTS giveleads (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
                   name VARCHAR(255) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30), requirements VARCHAR(255),
                   bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
+    db.execute('''CREATE TABLE IF NOT EXISTS buffer (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
+                  name VARCHAR(255) NOT NULL, district VARCHAR(100) NOT NULL, date VARCHAR(30), requirements VARCHAR(255),
+                  bgroup VARCHAR(5), phone VARCHAR(15), email VARCHAR(100), note text)''')
     db.execute('''CREATE TABLE IF NOT EXISTS feedback (id SERIAL PRIMARY KEY, unique_id CHAR(8) NOT NULL,
                   note text NOT NULL)''')
     db.execute('''CREATE TABLE IF NOT EXISTS helped (count INTEGER)''')
